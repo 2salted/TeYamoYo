0df9c97e96ed15fa3dd88779b44b9d14862f260f
2salted <noamgraf123@gmail.com>
2024-06-24 16:36:53 +0000

    major style update

--- App.tsx
+++ App.tsx
@@ -1,11 +1,60 @@
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { users } from "./localJSON";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+type Users = {
+  firstName: string;
+  lastName: string;
+  userType: string;
+  id: string;
+  pfp: any;
+};
+
+const { width } = Dimensions.get("window");
+
+export default function Main(): React.JSX.Element {
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 bg-white">
       <StatusBar style="auto" />
+      <FlatList
+        data={users}
+        renderItem={({ item }: { item: Users }) => (
+          <View className="w-1/2 items-center justify-center">
+            <Image source={item.pfp} style={styles.image} />
+            <Text>
+              {item.firstName} {item.lastName}
+            </Text>
+          </View>
+        )}
+        keyExtractor={(item: Users) => item.id}
+        numColumns={2}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  item: {
+    backgroundColor: "#f0f0f0",
+    padding: 10,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    height: width / 2,
+    marginBottom: 10,
+  },
+});
---
+++ localJSON.js
@@ -0,0 +1,25 @@
+let users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    userType: "1",
+    id: "29746583745",
+    pfp: require("./assets/pic1.jpg"),
+  },
+  {
+    firstName: "Goeff",
+    lastName: "gautier",
+    userType: "1",
+    id: "4953077348",
+    pfp: require("./assets/pic2.jpg"),
+  },
+  {
+    firstName: "Keith",
+    lastName: "Mckenna",
+    userType: "1",
+    id: "4953077348",
+    pfp: require("./assets/pic2.jpg"),
+  },
+];
+
+export { users };
